29bae163845f08be6d87272f259ba68406673e9a
Robins-2603 <robinsforwork@gmail.com>
2022-07-20 22:24:52 +0530

    added: UC1

--- src/com/addressBook/AddressBookMain.java
+++ src/com/addressBook/AddressBookMain.java
@@ -2,6 +2,24 @@ package com.addressBook;
 
 public class AddressBookMain {
     public static void main(String[] args) {
-        System.out.println("-----welcome to address book program--------");
-    }
+            System.out.println("--------Welcome to Address Book Program---------");
+            //Creating object
+            Contacts contacts = new Contacts();
+            contacts.setFirstName("Robins");
+            System.out.println("First Name: "+contacts.getFirstName());
+            contacts.setLastName("chandna");
+            System.out.println("Last Name: "+contacts.getLastName());
+            contacts.setAddress("vijay nagar");
+            System.out.println("Address: "+contacts.getAddress());
+            contacts.setCity("Ghaziabad");
+            System.out.println("City: "+contacts.getCity());
+            contacts.setState("uttarPradesh");
+            System.out.println("State: "+contacts.getState());
+            contacts.setZipCode("201009");
+            System.out.println("Pin Code: "+contacts.getZipCode());
+            contacts.setContactNumber("1234567890");
+            System.out.println("Contact Number: "+contacts.getContactNumber());
+            contacts.setEmailAddress("robindhaka26@gmail.com");
+            System.out.println("Email Address: "+contacts.getEmailAddress());
+        }
 }
